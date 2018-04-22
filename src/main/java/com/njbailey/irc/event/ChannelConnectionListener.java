f109a28a061201b2dd21f99b3e834d488549dc79
Shaun Houck <houcks08@gmail.com>
2018-04-22 14:18:57 -0400

    Adding documentation to code

--- src/main/java/com/njbailey/irc/event/ChannelConnectionListener.java
+++ src/main/java/com/njbailey/irc/event/ChannelConnectionListener.java
@@ -2,6 +2,11 @@ package com.njbailey.irc.event;
 
 import com.njbailey.irc.core.Channel;
 
+/**
+ * ChannelConnectionLister represents a 
+ * Listener for connections joining and leaving
+ * a channel.
+ */
 public interface ChannelConnectionListener {
     void joined(Channel channel);
 
